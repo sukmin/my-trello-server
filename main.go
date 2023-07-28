9506f7e88b39744589e8e4441bae36ab394f778b
killroad <killroad@gmail.com>
2023-07-28 15:42:40 +0900

    기본인증추가

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/gofiber/fiber/v2/middleware/skip"
 )
 
 func main() {
@@ -16,8 +18,23 @@ func main() {
 		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
 	}))
 
+	basicAuthHandler := basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"test": "test",
+		},
+	})
+	server.Use(skip.New(basicAuthHandler, func(ctx *fiber.Ctx) bool {
+		return ctx.Method() == fiber.MethodGet && ctx.OriginalURL() == "/health" //기본인증 제외정보
+	}))
+
 	server.Get("/health", func(c *fiber.Ctx) error {
 		body := `{"health":"OK"}`
+		c.Set("content-type", "application/json; charset=utf-8")
+		return c.SendString(body)
+	})
+
+	server.Get("/test", func(c *fiber.Ctx) error {
+		body := `{"test":"test"}`
 		c.Set("content-type", "application/json; charset=utf-8")
 		return c.SendString(body)
 	})
